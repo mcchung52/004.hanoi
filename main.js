aa3d634059554f1ee2442101f370db942c35b685
mcchung52 <mcchung52@hotmail.com>
2015-10-31 01:36:41 -0700

    working version

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function init() {
 }
 
 function drawDiscs() {
-	$('.tower').empty();
+	$('.discBag').empty();
 	$('.tower').css('padding-left',0);
 	$('.tower').css('padding-right',0);
 
@@ -20,25 +20,23 @@ function drawDiscs() {
 	var n = $('#discno').val();
 
   if (n>7) {
-  	$('.tower').css('padding-left',(n-7)*5+'px');//set min width of tower higher
-  	$('.tower').css('padding-right',(n-7)*5+'px');
+  	$('.tower').css('padding-left',(n-7)*10+'px');//set min width of tower higher
+  	$('.tower').css('padding-right',(n-7)*10+'px');
   }
 
-	var minsize = 30; //30%
-	var startOffset = 33;
-	var discBag = $('<div>');
+	var minsize = 30 - 5*(n>7?n-7:0); //30%
+	var startOffset = 33 + 5*(n>7?n-7:0);
 	while (n>0) {
 		var disc = $('<div>');
 		disc.addClass('disc');
 		disc.attr('id',n);
 		disc.css('margin-left',(startOffset - 5*(n-1))+'%'); //depending on disc #, size changes
 		disc.css('width',(minsize + 10*(n-1))+'%');
-		//disc.attr('background-color',);
+		disc.css('background-color','#'+Math.floor(Math.random()*16*16*16).toString(16));
 		disc.text(n);
-		discBag.prepend(disc);
+		$('#left').find('.discBag').prepend(disc); //tower->discBag->disc
 		n--;	
 	}
-	$('#left').prepend(discBag);
 }
 
 function towerClicked(event) {
@@ -50,28 +48,29 @@ function towerClicked(event) {
 		}
 		else {
 			//otherwise, move here, BUT IF IT ALLOWS
-			if ($(this).has('.disc').length==0) {//if no disc, move here
+			if ($(this).find('.disc').length==0) {//if no disc, move here
 				$seldisc.removeClass('selected');
-				$(this).find('').prepend($seldisc);
+				$(this).find('.discBag').prepend($seldisc);
 			}
 			else {//if there's disc, check against top
 				if (Number($(this).find('.disc:first-child').attr('id')) > Number($seldisc.attr('id'))) {
 					$seldisc.removeClass('selected');
-					$(this).prepend($seldisc);
+					$(this).find('.discBag').prepend($seldisc);
 				}
 			}
 		}
 	}
 	else {
-		if ($(this).has('.disc').length) {//selected here and there's disc
+		if ($(this).find('.disc').length) {//selected here and there's disc
 			//select top
 			$(this).find('.disc:first-child').addClass('selected');
 		}
 	}
 	//winning condition
 	if ($('#right').find('.disc').length==$('#discno').val()) {
-		console.log('solved!!');
+		alert('Solved!!');
 		$('#discno').prop('disabled',false);//unlock input box
 		$('#discno').focus();
+		$('.discBag').empty();
 	}
 }
